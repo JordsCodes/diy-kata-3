621a894d3a7fd4d5dfd43529a488645d54ef4fe8
Jordan <j.r.noble@live.co.uk>
2023-03-05 15:57:12 +0000

    humanCatDogYears passed

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,24 @@
-const humanCatDogYears = (number) => {};
-
+const humanCatDogYears = (number) => {
+  let counter = 1;
+  let catyears = 0;
+  let dogyears = 0;
+  do {
+    if (counter === 1) {
+      dogyears += 15;
+      catyears += 15;
+      counter += 1;
+    }
+    if (counter === 2) {
+      dogyears += 9;
+      catyears += 9;
+      counter += 1;
+    }
+    if (counter >= 3) {
+      dogyears += 5;
+      catyears += 4;
+      counter += 1;
+    }
+  } while (counter <= number);
+  return [number, catyears, dogyears];
+};
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  it("returns human, cat, dog years respectively", () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    expect(humanCatDogYears(12)).toEqual([12, 64, 74]);
+    expect(humanCatDogYears(15)).toEqual([15, 76, 89]);
+  });
+});
