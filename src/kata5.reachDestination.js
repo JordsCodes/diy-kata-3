0fc67faa6ab9799dd36b5264058dafc315c1532d
Jordan <j.r.noble@live.co.uk>
2023-03-05 20:01:07 +0000

    reachDestination passed

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,13 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  const time = distance / speed;
+  const decimal = ((distance / speed) % 1).toFixed(1);
 
+  if (decimal === "0.5") {
+    return `I should be there in ${time} hours.`;
+  }
+  if (decimal < 0.5) {
+    return `I should be there in ${Math.floor(time) + 0.5} hours.`;
+  }
+  return `I should be there in ${Math.floor(time) + 1} hours.`;
+};
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,9 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  it("returns string with estimated time of arrival", () => {
+    expect(reachDestination(65, 10)).toBe("I should be there in 6.5 hours.");
+    expect(reachDestination(44, 10)).toBe("I should be there in 4.5 hours.");
+    expect(reachDestination(87, 10)).toBe("I should be there in 9 hours.");
+  });
 });
