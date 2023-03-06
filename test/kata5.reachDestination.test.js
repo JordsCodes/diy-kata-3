37b0c6fea0f03e7aa19c142a601257a57336577b
Jordan <j.r.noble@live.co.uk>
2023-03-06 17:02:27 +0000

    kata 5 test update

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -3,7 +3,7 @@ const { reachDestination } = require("../src");
 describe("reachDestination", () => {
   it("returns string with estimated time of arrival", () => {
     expect(reachDestination(65, 10)).toBe("I should be there in 6.5 hours.");
-    expect(reachDestination(44, 10)).toBe("I should be there in 4.5 hours.");
-    expect(reachDestination(87, 10)).toBe("I should be there in 9 hours.");
+    expect(reachDestination(44, 20)).toBe("I should be there in 2.5 hours.");
+    expect(reachDestination(90, 25)).toBe("I should be there in 4 hours.");
   });
 });
