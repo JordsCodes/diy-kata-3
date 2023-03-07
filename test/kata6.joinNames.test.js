7b43dd4c34dae7365ffa87576897dac92ed54583
Jordan <j.r.noble@live.co.uk>
2023-03-07 20:32:44 +0000

    joinNames passed

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,17 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  it("returns string of names, seperated by commas and an ampersand", () => {
+    expect(
+      joinNames([{ name: "Bart" }, { name: "Lisa" }, { name: "Maggie" }])
+    ).toBe("Bart, Lisa & Maggie");
+    expect(
+      joinNames([
+        { name: "Cartman" },
+        { name: "Stan" },
+        { name: "Kyle" },
+        { name: "Kenny" },
+      ])
+    ).toBe("Cartman, Stan, Kyle & Kenny");
+  });
 });
